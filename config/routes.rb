b31e23d2e8e3bbc81c671260cb235e5e0df71dd1
thedamfr <dam.cavailles@gmail.com>
2015-03-14 20:59:16 +0100

    Added routes for Sidekiq UI

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,9 @@ Rails.application.routes.draw do
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
+	require 'sidekiq/web'
+	mount Sidekiq::Web => '/sidekiq'
+
   namespace :api, defaults: {format: 'json'} do
 
     namespace :v1 do
