2ca739344db10795840da785ec61c8b4249f8b37
thedamfr <dam.cavailles@gmail.com>
2015-03-14 23:35:07 +0100

    Broke ask user firstname

--- app/models/task_broker.rb
+++ app/models/task_broker.rb
@@ -4,11 +4,19 @@ class TaskBroker
     unless user.is_welcomed then
       welcome_user user
     end
+    if user.is_welcomed then
+      welcome_ask_user_firstname user
+    end
   end
 
   def welcome_user(user)
     puts "WelcomeUserJob queued"
     WelcomeUserJob.perform_later user
+  end
+
+  def welcome_ask_user_firstname(user)
+    puts "WelcomeAskUserFirstnameJob Queued"
+    WelcomeAskUserFirstnameJob.perform_later user
   end
 
 end
