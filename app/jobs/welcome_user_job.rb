e3ecdf466be972afef5c2c14b4b9ff2863189359
thedamfr <dam.cavailles@gmail.com>
2015-03-14 15:30:16 +0100

    fixed welcome job

--- app/jobs/welcome_user_job.rb
+++ app/jobs/welcome_user_job.rb
@@ -2,16 +2,18 @@ class WelcomeUserJob < ActiveJob::Base
   queue_as :default
 
   def perform(user)
+
     client = Twitter::REST::Client.new do |config|
       config.consumer_key        = ENV['CONSUMER_KEY']
       config.consumer_secret     = ENV['CONSUMER_SECRET']
       config.access_token        = ENV['ACCESS_TOKEN']
       config.access_token_secret = ENV['ACCESS_TOKEN_SECRETx']
     end
+
     client.follow(user.tw_handle)
     client.create_direct_message(user.tw_handle, 'Bienvenue sur Selfie On The Fly !')
-    client.direct_message(user.tw_handle, 'Nous allons configurer ton compte ensemble')
-    client.direct_message(user.tw_handle, 'A tout moment tu peux repondre "skip" pour passer à la question suivante')
+    client.create_direct_message(user.tw_handle, 'Nous allons configurer ton compte ensemble')
+    client.create_direct_message(user.tw_handle, 'A tout moment tu peux repondre "skip" pour passer à la question suivante')
 
     user.is_welcomed = true
 
