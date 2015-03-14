f50647f66794d91adff54c4969e481f3d666039b
Maxime Lenne <maxime.lenne@gmail.com>
2015-03-14 20:14:29 +0100

    ajout update dans les routes

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   namespace :api, defaults: {format: 'json'} do
 
     namespace :v1 do
-      resources :users, :only => [:index, :create, :show]
+      resources :users, :only => [:index, :create, :show, :update]
     end
 
   end
