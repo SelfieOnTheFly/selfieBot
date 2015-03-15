39c30069af4a69a53781aba3d3e6794ca148133c
thedamfr <dam.cavailles@gmail.com>
2015-03-15 02:24:14 +0100

    Name ?

--- app/models/task_broker.rb
+++ app/models/task_broker.rb
@@ -4,8 +4,11 @@ class TaskBroker
     unless user.is_welcomed then
       welcome_user user
     end
-    if user.is_welcomed then
+    if user.is_welcomed and user.firstname == nil then
       welcome_ask_user_firstname user
+    end
+    if user.firstname != nil and user.name == nil then
+      welcome_ask_user_name user
     end
   end
 
@@ -19,16 +22,32 @@ class TaskBroker
     WelcomeAskUserFirstnameJob.perform_later user
   end
 
+  def welcome_ask_user_name(user)
+    puts "WelcomeAskUserNameJob Queued"
+    WelcomeAskUserNameJob.perform_later user
+  end
+
   def on_dm(event)
 
     sender = User.find_by_tw_id(event.sender.id.to_s)
-    if sender != nil and sender.is_welcomed and sender.firstname == nil
-      if event.text.downcase.include? "skip"
-        sender.firstname = ""
-        sender.save
-      else
-        sender.firstname = event.text.capitalize
-        sender.save
+    if sender != nil
+      if sender.is_welcomed and sender.firstname == nil
+        if event.text.downcase.include? "skip"
+          sender.firstname = ""
+          sender.save
+        else
+          sender.firstname = event.text.capitalize
+          sender.save
+        end
+      end
+      if sender.firstname != nil and sender.name == nil
+        if event.text.downcase.include? "skip"
+          sender.name = ""
+          sender.save
+        else
+          sender.name = event.text.capitalize
+          sender.save
+        end
       end
     end
   end
