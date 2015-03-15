9b68026900e6f645f24ce8f16e367541a7c9cde2
thedamfr <dam.cavailles@gmail.com>
2015-03-15 01:54:09 +0100

    JE TE RU

--- Procfile
+++ Procfile
@@ -1,2 +1,3 @@
 web: bundle exec thin start -p $PORT
 worker: bundle exec sidekiq
+stream: bundle exec thin start -R eventmachine.ru
--- config/initializers/twitter.rb
+++ config/initializers/twitter.rb
@@ -5,26 +5,3 @@ $twitter = Twitter::REST::Client.new do |config|
   config.access_token        = ENV['TWITTER_ACCESS_TOKEN']
   config.access_token_secret = ENV['TWITTER_ACCESS_TOKEN_SECRET']
 end
-
-EM.schedule do
-
-  $stream = Twitter::Streaming::Client.new do |config|
-    config.consumer_key        = ENV['TWITTER_CONSUMER_KEY']
-    config.consumer_secret     = ENV['TWITTER_CONSUMER_SECRET']
-    config.access_token        = ENV['TWITTER_ACCESS_TOKEN']
-    config.access_token_secret = ENV['TWITTER_ACCESS_TOKEN_SECRET']
-  end
-
-  $stream.user do |event|
-    case event
-      when Twitter::Tweet
-        puts "It's a tweet!"
-      when Twitter::DirectMessage
-        ::TaskBroker.new.on_dm event
-      when Twitter::Streaming::StallWarning
-        warn "Falling behind!"
-    end
-  end
-
-end
-
