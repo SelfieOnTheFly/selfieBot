1ad1ab5a80a8e8743975fbdae46770bacbe2b1e2
thedamfr <dam.cavailles@gmail.com>
2015-03-15 14:19:03 +0100

    Tusais faire des quotes ?

--- app/jobs/welcome_conclude_job.rb
+++ app/jobs/welcome_conclude_job.rb
@@ -4,10 +4,10 @@ class WelcomeConcludeJob < ActiveJob::Base
   def perform(user)
 
     unless user.mail.include? "@"
-      $twitter.create_direct_message(user.tw_handle, 'Je ne peux pas créer de compte sans e-mail, #{user.firstname}')
+      $twitter.create_direct_message(user.tw_handle, "Je ne peux pas créer de compte sans e-mail, #{user.firstname}")
       $twitter.create_direct_message(user.tw_handle, 'Visite notre site web http://swlille.fr')
     else
-      $twitter.create_direct_message(user.tw_handle, 'Bienvenue sur SelfieOnTheFly #{user.firstname} !')
+      $twitter.create_direct_message(user.tw_handle, "Bienvenue sur SelfieOnTheFly #{user.firstname} !")
     end
 
 
